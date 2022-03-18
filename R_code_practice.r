c3c7f0825048c57ee4f281210fe6cc4ce874c849
positroniQuo <100956831+positroniQuo@users.noreply.github.com>
2022-03-18 17:40:20 +0100

    Update R_code_practice.r

--- R_code_practice.r
+++ R_code_practice.r
@@ -1,1 +1,11 @@
 library( raster )
+setwd("C:/lab/") # windows
+l2011 <- brick("p224r63_2011.grd")
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100) #scelgo una scala di colori adeguata
+plot(l2011, col=cl) #visualizzo l'immagine con i colori giusti
+ # b1 = blu
+# b2 = verde
+# b3 = rosso
+# b4 = infrarosso vicino NIR
+
+# Colori delle diverse bande
