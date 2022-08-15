815b3de6e59a2a15181ca3e79bc5e8b0ab05cfaa
positroniQuo <100956831+positroniQuo@users.noreply.github.com>
2022-08-15 18:12:30 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -1,1 +1,15 @@
+# Author: Filippo Arceci
+
+# This code processes images taken from sentinel2A/B of the in.cal system (sources of data: EarthExplorer, Copernicus) an abandoned quarry repurposed to be a natural reserve,
+# water storage and floodplain area near Rimini, next to the Marecchia river(link: https://albopretorio.comune.rimini.it/web/trasparenza/papca-ap?p_
+# p_id=jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=downloadAllegato&p_p_cacheability
+# =cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&_jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet_downloadSigned=false&_jcitygovalbopubblicazioni_
+# WAR_jcitygovalbiportlet_id=26621&_jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet_action=mostraDettaglio&_jcitygovalbopubblicazioni_WAR_jcitygovalbiportlet_
+# fromAction=recuperaDettaglio).
+# It is also a part of the Natura2000 network, and a very important wet area fot migratory birds and local fauna ( link: https://www.comune.rimini.it/servizi/ambiente/aree-naturali-protette).
+# However, direct observations showed a complete drought of the area in recent times, which nullifies the above mentioned functions of the area.
+# To have a better idea of what may have happened, it could be useful to analyse recent data (2015-2022) and search for patterns of change.
+# Since july is the dryest month of the year locally (link: https://it.climate-data.org/europa/italia/emilia-romagna/rimini-1176/), 
+# it will be the month from which images will be analysed.
+# Data include all the 10 mts resolution bands avaiable: RGB and NIR.
 
