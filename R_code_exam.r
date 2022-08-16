25d8e7926f859ea11689b1e71f3e7c8c58394f12
positroniQuo <100956831+positroniQuo@users.noreply.github.com>
2022-08-16 12:51:21 +0200

    Update R_code_exam.r

--- R_code_exam.r
+++ R_code_exam.r
@@ -25,16 +25,45 @@ library(rgdal)
 setwd("D:/sntl_data")
 
 all_sntl <- list.files(pattern="T32TQP")
-# importing files as they have been downloaded, from the working directory [alllst - all landsat]
+# importing files as they have been downloaded, from the working directory [allsntl - all Sentinel]
 
-sntl <- matrix(all_sntl, nrow = 8, ncol = 4, byrow=TRUE)
+sntl <- lapply(all_sntl, raster)
+# the data comes in .jp2 format, so i can use the "raster" function on it to process and plot the images with 
+# other "raster" package functions
+
+sntl <- matrix(sntl, nrow = 8, ncol = 4, byrow=TRUE)
 # making the unsorted data a matrix helps with elaboration:
 # rows are for years (1=2015, 2=2016...)
 # columns are for bands (1=B2, 2=B3, 3=B4, 4=B8)
 
-lst_sntl <- paste0 ('lst', sprintf ("%02d", as.numeric(15:22))) 
-# the last step to obtain the sorted bands for each year is to make a matrix of fitting names to assign them to [lstNN - landsat (years 00s-21s)]
+lst_sntl <- paste0 ('sntl', sprintf ("%02d", as.numeric(15:22))) 
+# the last step to obtain the sorted bands for each year is to make a matrix of fitting names to assign them to [sntlNN - Sentinel (years '15-'22)]
 
 for (i in seq_along (lst_sntl)) {assign (lst_sntl[[i]], stack (sntl[i,]))}
 # the ordered data is assigned to the corresponding object stacked, so now it is usable for plotting
 # (for example "sntl16" will contain a stack of bands 2, 3, 4, and 8 from july 2016)
+
+plotRGB(sntl16, r=3, g=2, b=1, stretch="lin")
+# plot test on a random image from the dataset. Everything seems fine.
+# the image will also be used for freehand tracing of the cutting guide
+
+mask <- drawExtent(show=T, col = "red")
+# cutting the images to only include the study area requires freehand drawing of a rectangle to sample
+# a smaller portion of the images downloaded to fit only the study area, with the "drawExtent() function.
+# First, I create a "mask" object,  whose dimentions will be used as a reference for cutting all the layers
+# later. Since the image is very big compared to the study area the cycle of drawing and cutting will be
+# repeated more than once to get more and more precise cuts, until it fits just right.
+
+lst_sntl_c <- paste0("sntl_c",sprintf ("%02d", as.numeric(15:22)))
+# creating new names for the cut images to be assigned at [lst_sntl_c = list of sentinel-cut]
+
+lst_sntl <- c(sntl15, sntl16, sntl17, sntl18, sntl19, sntl20, sntl21, sntl22)
+# let's regroup the individual stacks into a list to cut and then reassign them at the new variables
+
+for (i in seq_along (lst_sntl_c)) {assign (lst_sntl_c[i], crop(lst_sntl[[i]],mask))}
+# a simple loop to do the previously stated actions: cutting every stack using "mask" dimentions and assign it to the new variable
+# this procedure is repeated until the resulting images include only the In.Cal.System
+
+plotRGB(sntl_c16, r=3, g=2, b=1, stretch="lin")
+#controlling the results
+
